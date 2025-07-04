085a61da421cc777efd0e382af36f9ec294b1616
THEREALVANHEL <vishalkumarreddy01@gmail.com>
2025-07-04 06:07:23 +0530

    Create media_links.py

---
+++ assets/media_links.py
@@ -0,0 +1,11 @@
+# assets/media_links.py
+
+WELCOME_GIF = (
+    "https://cdn.discordapp.com/attachments/1370993458700877964/"
+    "1375089295257370624/image0.gif"
+)
+
+LEAVE_GIF = (
+    "https://cdn.discordapp.com/attachments/1351560015483240459/"
+    "1368427641564299314/image0.gif"
+)
